9979174a33385ccfef1d5c18953886e241951c1a
Yury Shakhmatov <shufflepkt@yandex.ru>
2023-02-03 17:52:58 +0100

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    @Test
+    public void addTest() {
+        String name = "Ivan", phoneNumber = "+79059999999";
+        int expected = 1;
+        var phoneBook = new PhoneBook();
+
+        var result = phoneBook.add(name, phoneNumber);
+
+        Assertions.assertEquals(expected, result);
+    }
 }
